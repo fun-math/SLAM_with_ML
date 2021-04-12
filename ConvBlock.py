7145bdb135071d10759828c81c15a694eeedb60e
fun-math <mallikakumar1729@gmail.com>
2021-04-13 00:28:38 +0530

    ConvBlock and VGG

---
+++ ConvBlock.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import tensorflow.keras.layers as nn
+import numpy
+import cv2
+
+class ConvBlock(tf.keras.layers.Layer) :
+	def __init__(self, layers, channels,logits=False,in_shape=None) :
+		super(ConvBlock, self).__init__()
+		self.layers=layers
+		self.channels=channels
+		self.logits=logits
+		self.convs=[]
+		for i in range(self.channels-1) :
+			self.convs+=[nn.Conv2D(self.channels,3,1,padding='same',activation='relu')]
+		self.convs+=[nn.Conv2D(self.channels,3,1,padding='same')]
+		# if in_shape!=None :
+		# 	self.convs[0]=nn.Conv2D(self.channels,3,1,padding='same',activation='relu',input_shape=in_shape)
+		self.pool=nn.MaxPool2D(2,2)
+		if not self.logits :
+			self.relu=nn.ReLU()
+
+	def call(self,x) :
+		for i in range(self.channels) :
+			x=self.convs[i](x)
+		x=self.pool(x)
+		if not self.logits :
+			x=self.relu(x)
+		return x
---
+++ VGG.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import tensorflow.keras.layers as nn
+import numpy
+import cv2
+
+from ConvBlock import *
+
+class VGG(tf.keras.Model) :
+	def __init__(self,in_shape=(224,224,3)) :
+		super(VGG,self).__init__()
+		self.in_shape=in_shape
+		self.conv_build=nn.Conv2D(64,3,1,padding='same',activation='relu',input_shape=(224,224,3))
+		self.block1=ConvBlock(1,64,in_shape=self.in_shape)
+		self.block2=ConvBlock(2,128)
+		self.block3=ConvBlock(3,256)
+		self.block4=ConvBlock(3,512)
+		self.block5=ConvBlock(3,512,logits=True)
+		self.norm=nn.Lambda(lambda x : tf.math.l2_normalize(x,axis=-1))
+
+	def call(self,x) :
+		x=self.conv_build(x)
+		x=self.block1(x)
+		x=self.block2(x)
+		x=self.block3(x)
+		x=self.block4(x)
+		x=self.block5(x)
+		x=self.norm(x)
+
+		return x
+
+if __name__=='__main__':
+	vgg=VGG()
+	vgg.summary()
