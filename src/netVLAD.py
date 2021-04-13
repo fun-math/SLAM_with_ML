b658fdb4b13bf4ff8a516ef8a2447ce350051371
fun-math <mallikakumar1729@gmail.com>
2021-04-13 15:37:58 +0530

    netVLAD

---
+++ src/netVLAD.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import tensorflow.keras.layers as nn
+import numpy
+import cv2
+
+from VGG import *
+from netVLADlayer import *
+
+class netVLAD(tf.keras.Model) :
+	def __init__(self,):
+		super(netVLAD,self).__init__()
+		self.vgg=VGG()
+		self.netVLADlayer=netVLADlayer()
+
+	def call(self,x):
+		x=vgg(x)
+		x=netVLADlayer(x)
+
+		return x
+
+if __name__=='__main__':
+	m=netVLAD()
+	x=np.random.rand([224,224,3])
+	y=m(x)
