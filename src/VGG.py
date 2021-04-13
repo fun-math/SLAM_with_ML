4e3d4e411de984e3ba71cb730f7d3023ebba76aa
fun-math <mallikakumar1729@gmail.com>
2021-04-13 22:24:20 +0530

    fixed bugs

--- src/VGG.py
+++ src/VGG.py
@@ -8,7 +8,7 @@ from ConvBlock import *
 class VGG(tf.keras.Model) :
 	def __init__(self,in_shape=(224,224,3)) :
 		super(VGG,self).__init__()
-		self.in_shape=in_shape
+		self.input_layer=tf.keras.layers.Input(shape=[None,224,224,3])
 		self.conv_build=nn.Conv2D(64,3,1,padding='same',activation='relu',input_shape=(224,224,3))
 		self.block1=ConvBlock(1,64,in_shape=self.in_shape)
 		self.block2=ConvBlock(2,128)
@@ -16,6 +16,7 @@ class VGG(tf.keras.Model) :
 		self.block4=ConvBlock(3,512)
 		self.block5=ConvBlock(3,512,logits=True)
 		self.norm=nn.Lambda(lambda x : tf.math.l2_normalize(x,axis=-1))
+		self.out=self.call(self.input_layer)
 
 	def call(self,x) :
 		x=self.conv_build(x)
@@ -30,5 +31,5 @@ class VGG(tf.keras.Model) :
 
 if __name__=='__main__':
 	vgg=VGG()
-	vgg.build((None,224,224,3))
+	# vgg.build((None,224,224,3))
 	vgg.summary()
