62399b6a9574492b113ec059ca4d5becfb04379a
fun-math <mallikakumar1729@gmail.com>
2021-04-18 18:04:56 +0530

    overview of hfnet code

---
+++ src/HFnet.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import tensorflow.keras.layers as nn
+import numpy as np
+import cv2
+
+from tf.keras.applications import MobileNetV2
+#import netvlad and superpoint
+
+class HFnet(tf.keras.Model) :
+	def __init__(input_shape=(640,480,3),alpha=0.75) :
+		super(HFnet,self).__init__()
+
+		self.input_shape=input_shape
+		self.alpha=alpha
+		self.backbone=MobileNetV2(input_shape=self.input_shape,alpha=self.alpha,
+						include_top=False,weights='imagenet')
+		self.hidden=backbone.get_layer()#pass layer name
+		self.detector_head=None #detector head
+		self.descriptor_head=None #descriptor head
+		self.netvladlayer=None #netvladlayer
+
+	def call(self,x) :
+		x=self.backbone(x)
+
+		global_descriptor=self.netvladlayer(x)
+		local_descriptor=self.descriptor_head(self.hidden)
+		key_points=self.detector_head(self.hidden)
+
+		return [global_descriptor,local_descriptor,key_points]
+
