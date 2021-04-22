7ec81f5f02b6f1a99341c6124023ac0698ec39a0
fun-math <mallikakumar1729@gmail.com>
2021-04-22 23:19:45 +0530

    Loss.py and updated HFnet

--- src/HFnet.py
+++ src/HFnet.py
@@ -14,6 +14,7 @@ class HFnet(tf.keras.Model) :
 
 		self.input_shape=input_shape
 		self.alpha=alpha
+		self.block_size=8
 		self.backbone=MobileNetV2(input_shape=self.input_shape,alpha=self.alpha,
 						include_top=False,weights='imagenet')
 		self.hidden=backbone.get_layer()#pass layer name
@@ -21,12 +22,20 @@ class HFnet(tf.keras.Model) :
 		self.descriptor_head=Descriptor() #descriptor head
 		self.netvladlayer=None #netvladlayer
 
-	def call(self,x) :
+		self.loss_multipliers=tf.Variable(shape=(3,1),
+						initial_value=tf.ones(shape=(3,1)))#Loss multipliers
+
+
+	def call(self,x,training=None) :
 		x=self.backbone(x)
 
 		global_descriptor=self.netvladlayer(x)
 		local_descriptor=self.descriptor_head(self.hidden)
 		key_points=self.detector_head(self.hidden)
 
-		return [global_descriptor,local_descriptor,key_points]
+		if training :
+			return [global_descriptor,local_descriptor,key_points,self.loss_multipliers]
 
+		#throw away the dustbin and return probabilistic scores
+		key_points_map=tf.nn.depth_to_space(np.array(key_points)[:,:,:,:-1],self.block_size)
+		return [global_descriptor,local_descriptor,key_points_map]
