e1c846f2f6e2815743e757eead823d3acd4e4bb7
fun-math <mallikakumar1729@gmail.com>
2021-04-23 19:08:54 +0530

    fixed bug in loss.py, multi labels in dataset.py

--- src/Loss.py
+++ src/Loss.py
@@ -1,31 +1,35 @@
 import tensorflow as tf 
 import numpy as np 
-import tf.random.normal as randnorm
 
 class Loss(tf.keras.losses.Loss) :
 	def __init__(self) :
-		pass
+		super(Loss,self).__init__(name='loss')
 
-	def call(y_true,y_pred) :
+	def call(self,y_true,y_pred) :
 		loss_multipliers=y_pred[-1]
 		coeffs=tf.math.exp(loss_multipliers)
 
-		loss_gdesc=tf.math.reduce_sum(tf.math.square(y_true[0]-y_pred[0]),axis=-1)
-		loss_ldesc=tf.math.reduce_sum(tf.math.square(y_true[1]-y_pred[1]),axis=-1)
-		loss_ldet=tf.keras.losses.categorical_crossentropy(y_true[2],y_pred[2])
+		loss_gdesc=tf.math.reduce_mean(
+					tf.math.reduce_sum(
+						tf.math.square(y_true[0]-y_pred[0]),axis=-1))
+		loss_ldesc=tf.math.reduce_mean(
+					tf.math.reduce_sum(
+						tf.math.square(y_true[1]-y_pred[1]),axis=-1))
+		loss_ldet=tf.math.reduce_mean(
+					tf.keras.losses.categorical_crossentropy(y_true[2],y_pred[2]))
 
-		loss=coeffs*np.array([loss_gdesc,loss_ldesc,loss_ldet])+tf.math.reduce_sum(loss_multipliers)
-
+		loss=coeffs[0]*loss_gdesc+coeffs[1]*loss_ldesc+coeffs[2]*loss_ldet+tf.math.reduce_sum(loss_multipliers)
 		return loss
+		
 
 if __name__=='__main__' :
 	loss=Loss()
-	x1=randnorm(shape=(3,4096))
-	y1=randnorm(shape=(3,4096))
-	x2=randnorm(shape=(3,80,80,64))
-	y2=randnorm(shape=(3,80,80,64))
-	x3=randnorm(shape=(3,80,80,65))
-	y3=randnorm(shape=(3,80,80,65))
-	mult=randnorm(shape=(3,1))
+	x1=tf.random.normal(shape=(3,4096))
+	y1=tf.random.normal(shape=(3,4096))
+	x2=tf.random.normal(shape=(3,80,80,64))
+	y2=tf.random.normal(shape=(3,80,80,64))
+	x3=tf.random.normal(shape=(3,80,80,65))
+	y3=tf.random.normal(shape=(3,80,80,65))
+	mult=tf.random.normal(shape=(3,1))
 	l=loss([x1,x2,x3],[y1,y2,y3,mult])
 	print(l)
--- src/dataset.py
+++ src/dataset.py
@@ -4,15 +4,20 @@ import cv2
 import glob
 
 #/media/ironwolf/students/amit/datasets/bdd100k/images/100k/train/
+#/media/ironwolf/students/amit/datasets/bdd100k/labels/100k/train/<label_type>
+# <label_type> is one of ['gdesc/','ldesc/','ldet/']
 
 class Dataset(tf.keras.utils.Sequence) :
-	def __init__(self,pre_path='/media/ironwolf/students/amit/datasets/bdd100k/',
-		post_path='/100k/',split='train',batch_size=64)
+	def __init__(self,pre_path="/media/ironwolf/students/amit/datasets/bdd100k/",
+		post_path='100k/',split='train/',batch_size=64) :
+		
 		self.pre_path=pre_path
 		self.post_path=post_path
 		self.split=split
+		self.batch_size=batch_size
+		self.label_types=['gdesc/','ldesc/','ldet/']
 		self.names=[full_name.split('/')[-1] 
-			for full_name in glob.glob(pre_path+'images'+post_path+split+'/*')]
+			for full_name in glob.glob(pre_path+'images/'+post_path+split+'*')]
 
 	def __len__(self) :
 		return np.ceil(len(self.names)/self.batch_size)
@@ -25,14 +30,16 @@ class Dataset(tf.keras.utils.Sequence) :
 	def __getitem__(self,idx) :
 		batch_names=self.names[idx*self.batch_size : (idx+1)*self.batch_size]
 
-		batch_x=np.array([self._imread(f'{self.pre_path}images{self.post_path}{self.split}/{name}')
+		batch_x=np.array([self._imread(f'{self.pre_path}images/{self.post_path}{self.split}{name}')
 				for name in batch_names])
-		batch_y=np.array([self._imread(f'{self.pre_path}labels{self.post_path}{self.split}/{name}')
-				for name in batch_names])
+		batch_y=[np.array([self._imread(f'{self.pre_path}labels/{self.post_path}{self.split}{label_type}{name}')
+					for name in batch_names])
+						for label_type in self.label_types]
 
 		return (batch_x,batch_y)
 		
 
 if __name__=='__main__' :
 	data=Dataset()
+	print(data.__len__())
 	#To be tested after generating labels successfully
