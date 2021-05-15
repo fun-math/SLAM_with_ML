2b5cb829e4917f7bcb523ee197c5e29710a9b362
Aanal Mahesh Sonara <62333645+Aanal2901@users.noreply.github.com>
2021-05-15 17:00:32 +0530

    Update ConvBlock.py

--- src/ConvBlock.py
+++ src/ConvBlock.py
@@ -4,19 +4,19 @@ import numpy as np
 import cv2
 
 class ConvBlock(tf.keras.layers.Layer) :
-	def __init__(self, layers, channels,logits=False,in_shape=None) :
-		super(ConvBlock, self).__init__()
-		self.layers=layers
+	def __init__(self, layers, channels,logits=False,in_shape=None) :		#definition of class
+		super(ConvBlock, self).__init__()					
+		self.layers=layers							# number of layers of this class instatiation is layers given during definition
 		self.channels=channels
 		self.logits=logits
 		self.convs=[]
 		for i in range(self.layers-1) :
-			self.convs+=[nn.Conv2D(self.channels,3,1,padding='same',activation='relu')]
+			self.convs+=[nn.Conv2D(self.channels,3,1,padding='same',activation='relu')] ## use as many convolution layers as defined.
 		self.convs+=[nn.Conv2D(self.channels,3,1,padding='same')]
 		# if in_shape!=None :
 		# 	self.convs[0]=nn.Conv2D(self.channels,3,1,padding='same',activation='relu',input_shape=in_shape)
 		self.pool=nn.MaxPool2D(2,2)
-		if not self.logits :
+		if not self.logits :			##After three layers, max pooling and then default activation function is ReLU.
 			self.relu=nn.ReLU()
 
 	def call(self,x) :
