986830ab64add832530055075a6dea0d5324dcd5
fun-math <mallikakumar1729@gmail.com>
2021-05-19 22:27:06 +0530

    Updated Losses

--- src/HFnet.py
+++ src/HFnet.py
@@ -3,38 +3,38 @@ import tensorflow as tf
 import tensorflow.keras.layers as nn
 import numpy as np
 import cv2
-
+print(tf.executing_eagerly())
 
 from netVLADlayer import netVLADlayer
 from tensorflow.keras.applications import MobileNetV2
 from detector import Detector
 from descriptor import Descriptor
-from Loss import Loss
+from Loss import *
 #import netvlad and superpoint
 
 class HFnet(tf.keras.Model) :
-    def __init__(self, input_shape=(640,480,3),alpha=0.75) :
+    def __init__(self, in_shape=(640,480,3),alpha=0.75) :
         super(HFnet,self).__init__()
 
         #self.input_shape=input_shape
         self.alpha=alpha
         self.block_size=8
-        self.backbone=MobileNetV2(input_shape=input_shape,alpha=self.alpha, include_top=False,weights='imagenet')
+        self.backbone=MobileNetV2(input_shape=in_shape,alpha=self.alpha, include_top=False,weights='imagenet')
 
         self.hidden=self.backbone.get_layer('block_6_project_BN').output#pass layer name
         self.detector_head=Detector() #detector head
         self.descriptor_head=Descriptor() #descriptor head
         self.netvladlayer=netVLADlayer(dim = 1280) #netvladlayer
 
-        self.loss_multipliers=tf.Variable(shape=(3,1),
-                        initial_value=tf.ones(shape=(3,1)))#Loss multipliers
-        self.compiled_loss = Loss()
-        self.optimizer = tf.keras.optimizers.RMSprop()
+        # self.loss_multipliers=tf.Variable(shape=(3,1),
+        #                 initial_value=tf.ones(shape=(3,1)))#Loss multipliers
+        # self.compiled_loss = Loss()
+        # self.optimizer = tf.keras.optimizers.RMSprop()
 
 
     def call(self,x,training=None) :
         x=self.backbone(x)
-        print(x.shape)
+        # print(x.shape)
  
         global_descriptor=self.netvladlayer(x)
         
@@ -42,17 +42,21 @@ class HFnet(tf.keras.Model) :
         key_points=self.detector_head(self.hidden)
 
         #if training :
-        return [global_descriptor,local_descriptor,key_points,self.loss_multipliers]
+        return [global_descriptor,local_descriptor,key_points]#,self.loss_multipliers
 
         #throw away the dustbin and return probabilistic scores
-        key_points_map=tf.nn.depth_to_space(key_points[:,:,:,:-1],self.block_size)
-        return [global_descriptor,local_descriptor,key_points_map, self.loss_multipliers]
+        # key_points_map=tf.nn.depth_to_space(key_points[:,:,:,:-1],self.block_size)
+        # return [global_descriptor,local_descriptor,key_points_map, self.loss_multipliers]
 
+    '''
     def train_step(self, data):
         x, y = data
-
+        # print('**************')
+        # print(len(y))
         with tf.GradientTape() as tape:
             y_pred = self(x, training = True)
+            # print('#########**************')
+            # print(len(y_pred))
             train_loss = self.compiled_loss(y, y_pred)
 
         gradients = tape.gradient(train_loss, self.trainable_variables)
@@ -69,29 +73,29 @@ class HFnet(tf.keras.Model) :
         if self.step==80000:
             optimizer.learning_rate.assign(0.00001)
         self.step = self.step + 1
-        
+      
+'''
 
+model = HFnet(in_shape = (45, 45, 3))
 
-model = HFnet(input_shape = (224, 224, 3))
+x = tf.random.normal((2, 45,45,3))
+x1=tf.random.normal(shape=(2,4096))
+x2=tf.random.normal(shape=(2,3,3,256))
+x3=tf.random.normal(shape=(2,3, 3, 65))
+# x4 = tf.random.normal(shape = (2, 3, 1))
+y = [x1, x2, x3]
 
-x = tf.random.normal((1, 224,224,3))
-x1=tf.random.normal(shape=(1,4096))
-x2=tf.random.normal(shape=(1,14,14,256))
-x3=tf.random.normal(shape=(1,14, 14, 65))
-x4 = tf.random.normal(shape = (1, 3, 1))
-y = [x1, x2, x3, x4]
+# y_pred = model(x)
+# print(len(y_pred))
+# print(y_pred[1].shape)
+# print(y_pred[2].shape)
 
-loss = Loss()
-
-y_pred = model(x)
-print(y_pred[1].shape)
-print(y_pred[2].shape)
-
-print(len(y))
-print(loss(y, y_pred))
-print(tf.__version__)
-#model.compile(optimizer = "RMSprop", loss = loss)
+# print(len(y))
+# print(loss(y, y_pred))
+# print(tf.__version__)
+model.compile(optimizer = "RMSprop", 
+	loss = [Loss_desc('gdesc'),Loss_desc('ldesc'),Loss_ldet()])
 
 model.train_step((x, y))
-#model.fit(x, y, epochs = 1, batch_size = 1, steps_per_epoch = 1)
+# model.fit(x, y, epochs = 1, batch_size = 2)
 
--- src/Loss.py
+++ src/Loss.py
@@ -1,14 +1,44 @@
 import tensorflow as tf 
 import numpy as np 
 
+class Loss_desc(tf.keras.losses.Loss) :
+	def __init__(self,name=None) :
+		super(Loss_desc,self).__init__(name=name)
+		self.loss_multiplier=tf.Variable(shape=(),initial_value=1.0)
+
+	def call(self,y_true,y_pred) :
+		loss_desc=tf.math.reduce_mean(
+					tf.math.reduce_sum(
+						tf.math.square(y_true-y_pred),axis=-1))
+
+		return (loss_desc+1)*self.loss_multiplier
+
+
+class Loss_ldet(tf.keras.losses.Loss) :
+	def __init__(self,name='ldet') :
+		super(Loss_ldet,self).__init__(name=name)
+		self.loss_multiplier=tf.Variable(shape=(),initial_value=1.0)
+
+	def call(self,y_true,y_pred) :
+		loss_ldet=tf.math.reduce_mean(
+					tf.keras.losses.categorical_crossentropy(y_true,y_pred))
+
+		return (loss_ldet+1)*self.loss_multiplier
+
+
 class Loss(tf.keras.losses.Loss) :
 	def __init__(self) :
 		super(Loss,self).__init__(name='loss')
+		self.loss_multipliers=tf.Variable(shape=(),initial_value=1)
 
 	def call(self,y_true,y_pred) :
 		loss_multipliers=y_pred[-1]
 		coeffs=tf.math.exp(loss_multipliers)
-
+		print(y_true.shape,y_pred.shape)
+		print('------------')
+		'''
+		for i in range(4) :
+			print(i,y_true[i].shape,y_pred[i].shape)
 		loss_gdesc=tf.math.reduce_mean(
 					tf.math.reduce_sum(
 						tf.math.square(y_true[0]-y_pred[0]),axis=-1))
@@ -19,17 +49,20 @@ class Loss(tf.keras.losses.Loss) :
 					tf.keras.losses.categorical_crossentropy(y_true[2],y_pred[2]))
 
 		loss=coeffs[0]*loss_gdesc+coeffs[1]*loss_ldesc+2*coeffs[2]*loss_ldet+tf.math.reduce_sum(loss_multipliers)
-		return loss
+		'''
+		return 0#loss
 
 
 if __name__=='__main__' :
-	loss=Loss()
+	
+	loss_gdesc=Loss_desc('gdesc')
+	loss_ldesc=Loss_desc('ldesc')
+	loss_ldet=Loss_ldet()
 	x1=tf.random.normal(shape=(3,4096))
 	y1=tf.random.normal(shape=(3,4096))
 	x2=tf.random.normal(shape=(3,80,80,64))
 	y2=tf.random.normal(shape=(3,80,80,64))
 	x3=tf.random.normal(shape=(3,80,80,65))
 	y3=tf.random.normal(shape=(3,80,80,65))
-	mult=tf.random.normal(shape=(3,1))
-	l=loss([x1,x2,x3],[y1,y2,y3,mult])
-	print(l)
+	
+	print(loss_gdesc(x1,y1),loss_ldesc(x2,y2),loss_ldesc(x3,y3))
