ab1c56e7f4f19f30d1d50ae29f08b83c19c1daa1
fun-math <mallikakumar1729@gmail.com>
2021-05-24 18:49:19 +0530

    fixed some bugs

--- src/HFnet.py
+++ src/HFnet.py
@@ -14,7 +14,7 @@ class HFnet(tf.keras.Model) :
     def __init__(self, in_shape=(640,480,3),alpha=0.75,mid=7) :
         super(HFnet,self).__init__()
 
-        #self.input_shape=input_shape
+        self.in_shape=in_shape
         self.alpha=alpha
         self.block_size=8
         self.base=MobileNetV2(input_shape=in_shape,include_top=False,alpha=0.75,
@@ -27,6 +27,7 @@ class HFnet(tf.keras.Model) :
         self.detector_head=Detector() #detector head
         self.descriptor_head=Descriptor() #descriptor head
         self.netvladlayer=netVLADlayer(dim = 1280) #netvladlayer
+        self.step=0
 
 
     def call(self,x,training=None) :
@@ -42,7 +43,7 @@ class HFnet(tf.keras.Model) :
 
         #throw away the dustbin and return probabilistic scores
         key_points_map=tf.nn.depth_to_space(key_points[:,:,:,:-1],self.block_size)
-        return [global_descriptor,local_descriptor,key_points_map, self.loss_multipliers]
+        return [global_descriptor,local_descriptor,key_points_map]
 
     
     def train_step(self, data):
@@ -66,11 +67,17 @@ class HFnet(tf.keras.Model) :
         if self.step==80000:
             optimizer.learning_rate.assign(0.00001)
         self.step = self.step + 1
+
+    def build_graph(self) :
+        x=tf.keras.Input(shape=self.in_shape)
+        return tf.keras.Model(inputs=[x],outputs=self.call(x))
       
 
 if __name__=='__main__' :
 
     model = HFnet(in_shape = (640, 480, 3),mid=7)
+    model.build((None,640,480,3))
+    # tf.keras.utils.plot_model(model.build_graph(),to_file='HFnet.png',expand_nested=True)
 
     x = tf.random.normal((2, 640,480,3))
     x1=tf.random.normal(shape=(2,4096))
