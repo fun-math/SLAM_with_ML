86e045051436e0dc6bcff65b51df73a2c4980cd1
fun-math <mallikakumar1729@gmail.com>
2021-05-29 20:07:24 +0530

    fixed bugs

--- src/detector.py
+++ src/detector.py
@@ -3,13 +3,12 @@ import tensorflow.keras.layers as nn
 import numpy as np
 import cv2
 
-class Detector(tf.keras.layers.Layer, axis) :
-	def __init__(self) :
+class Detector(tf.keras.layers.Layer) :
+	def __init__(self,axis=-1) :
 		super(Detector,self).__init__()
 		self.conv1=nn.Conv2D(256,3,1,padding='same',activation='relu')
 		self.conv2=nn.Conv2D(65,1,1,padding='same')
-		self.softmax=self.softmax()
-    self.axis = axis
+		self.axis = axis
 
 	def call(self,x) :
 		x=self.conv1(x)
@@ -17,11 +16,11 @@ class Detector(tf.keras.layers.Layer, axis) :
 		x=self.Softmax(x)
 
 		return x
-  
+
 
 	def Softmax(self,x) :
-		m = np.max(x, axis = -1, keepdims = True)
-		lse = m + np.exp(np.sum(x - m, axis = -1, keepdims = True))
+		m = np.max(x, axis = self.axis, keepdims = True)
+		lse = m + np.log(np.sum(np.exp(x - m), axis = self.axis, keepdims = True))
 		soft = np.exp(x - lse)
 
 		return soft
@@ -30,6 +29,7 @@ class Detector(tf.keras.layers.Layer, axis) :
 
 if __name__=='__main__' :
 	desc=Detector()
-	x=tf.random.normal(shape=(1,120,120,96))
+	x=10*tf.random.normal(shape=(1,120,120,96))
 	y=desc(x)
 	print(y.shape)
+	print(tf.norm(np.sum(y,axis=-1)-np.ones(y.shape[:3]))/tf.norm(np.ones(y.shape[:3])))
