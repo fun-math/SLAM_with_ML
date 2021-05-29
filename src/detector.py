038fc072b955af2b7cb919d0c6099580e705d1fe
Aanal Mahesh Sonara <62333645+Aanal2901@users.noreply.github.com>
2021-05-29 19:22:04 +0530

    Add files via upload

--- src/detector.py
+++ src/detector.py
@@ -3,19 +3,30 @@ import tensorflow.keras.layers as nn
 import numpy as np
 import cv2
 
-class Detector(tf.keras.layers.Layer) :
+class Detector(tf.keras.layers.Layer, axis) :
 	def __init__(self) :
 		super(Detector,self).__init__()
 		self.conv1=nn.Conv2D(256,3,1,padding='same',activation='relu')
 		self.conv2=nn.Conv2D(65,1,1,padding='same')
-		self.softmax=nn.Softmax()
+		self.softmax=self.softmax()
+    self.axis = axis
 
 	def call(self,x) :
 		x=self.conv1(x)
 		x=self.conv2(x)
-		x=self.softmax(x)
+		x=self.Softmax(x)
 
 		return x
+  
+
+	def Softmax(self,x) :
+		m = np.max(x, axis = -1, keepdims = True)
+		lse = m + np.exp(np.sum(x - m, axis = -1, keepdims = True))
+		soft = np.exp(x - lse)
+
+		return soft
+    
+
 
 if __name__=='__main__' :
 	desc=Detector()
