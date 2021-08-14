5e2eda294e34657d9ba51ff2c382a10ef4f53bb2
fun-math <mallikakumar1729@gmail.com>
2021-08-14 12:16:00 +0530

    MLP

---
+++ SuperGlue/MLP.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import tensorflow.keras.layers as nn
+import numpy as np
+
+__all__=["MLP"]
+
+def MLP(channels, bn=True) :
+    
+    model = tf.keras.Sequential([])
+
+    for i in range(len(channels)) :
+        model.add(nn.Conv1D(channels[i],1))
+        if bn :
+            model.add(nn.BatchNormalization())
+        model.add(nn.ReLU())
+
+    return model
+
+if __name__=='__main__':
+    channels=[32,32,64]
+    model=MLP(channels)
+    model.build((None,1,10))
+    model.summary()
+    
